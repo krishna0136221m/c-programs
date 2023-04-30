ca89472256629135ec0c506f3300b6bb87b62398
KRISHNA <114530864+krishna0136221m@users.noreply.github.com>
2023-04-30 22:15:51 +0530

    Add files via upload

---
+++ array_questions.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void main()
+{
+/*
+//1 - to remove an element from an array.
+      int a[10],i,j,index;
+      printf("enter element of array\n");
+      for(i=0;i<10;i++)
+      {
+          scanf("%d",&a);
+      }
+      int key;
+      printf("enter key to remove\n");
+      scanf("%d",&key);
+      index=9;
+      for(int i=0;i<=index;i++)
+      {
+          if(a[i]==key)
+          {
+              for(j=i+1;j<=index;j++)
+              {
+                  a[j-1]=a[j];
+                  i--;
+                  index--;
+              }
+          }
+      }
+      printf("new array after removing element from array\n");
+      for(i=0;i<=index;i++)
+      {
+          printf("%d",a[i]);
+      }
+      getch();
+      */
+
+
+
+
+}
