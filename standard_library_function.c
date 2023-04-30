1d0bc57cac7411f95d8b6fd68f841ba7b1325b7f
KRISHNA <114530864+krishna0136221m@users.noreply.github.com>
2023-04-30 22:20:27 +0530

    Add files via upload

---
+++ standard_library_function.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+/*THIS IS ABOUT FUNCTION IN STRING.
+1- strlen();*/
+/*char a[10];
+printf("ENTER YOUR STRING");
+gets(a);
+printf("the string is %s\n",a);
+int len=strlen(a);
+printf("lenght of string is %d",len);
+*/
+
+
+
+/*2- strcpy();  - to copy any string
+char a[10];
+printf("enter your string");
+scanf("%s",a);
+char b[10];
+strcpy(b,a);
+printf("copied new string is %s",b);*/
+
+
+/*3- strcat(); - this is use to concat any two string
+char a[10];
+printf("enter first string\n");
+scanf("%s",a);
+char b[10];
+printf("enter your second string\n");
+scanf("%s",b);
+char c[10]="ccml";
+strcat(a,b);
+strcat(a,c);
+printf("concatenated string is %s",a);
+*/
+
+/*3-strcmp(); - this is used to compare string if string is eual it returns 0 and if string is not eqyual then it returns 1.
+
+char a[10];
+printf("enter first string\n");
+scanf("%s",a);
+char b[10];
+printf("enter second string\n");
+scanf("%s",b);
+int c=strcmp(a,b);
+if(c==0)
+{
+    printf("string is equal");
+}
+else
+{
+
+    printf("string is not equal");
+}
+*/
+getch();
+}
